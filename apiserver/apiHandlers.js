5721354526c3d95f9347bda852b6a98a436f9dae
Yasushi Asahi <asahi@MBP.local>
2018-06-19 07:31:22 +0900

    conect api container

---
+++ apiserver/apiHandlers.js
@@ -0,0 +1,125 @@
+'use strict'
+
+const path = require('path')
+const usersFilePath = path.join(__dirname, `../data/databases/userdata.json`)
+const galleryFilePath = path.join(__dirname, '../data/databases/gallerydata.json')
+const pictureDir = path.join(__dirname, '../dist/images/gallery/')
+const {
+  removePicture,
+  getGalleryDataAndFindTargetIndex,
+  renameFile,
+  writeFile,
+  getDatabase,
+  getFormData,
+  parseReqBody,
+  getHash,
+  getToken,
+  getUniqueStr
+} = require('./apiHelper.js')
+
+const signIn = async request => {
+  console.log('/api/SignInが呼ばれた')
+
+  const { userName: reqUserName, password: reqPassword } = await parseReqBody(request)
+  const users = await getDatabase(usersFilePath)
+  for (const user of users) {
+    if (user.userName === reqUserName) {
+      throw { errMsg: `${reqUserName}は既に登録されています` }
+    }
+  }
+  const password = getHash(reqPassword)
+  const token = getToken(reqUserName)
+  users.push({ userName: reqUserName, password, token })
+  await writeFile(usersFilePath, users)
+  return { userName: reqUserName, token }
+}
+
+const logIn = async request => {
+  console.log('/api/LogInが呼ばれた')
+
+  const { userName: reqUserName, password: reqPassword } = await parseReqBody(request)
+  const users = await getDatabase(usersFilePath)
+  const targetIndex = users.findIndex(user => user.userName === reqUserName)
+  if (targetIndex === -1) throw { errMsg: `${reqUserName}は登録されていません` }
+  if (users[targetIndex].password !== getHash(reqPassword)) {
+    throw { errMsg: `${reqUserName}さんのパスワードと一致しません` }
+  }
+  const newToken = getToken(reqUserName)
+  users[targetIndex].token = newToken
+  await writeFile(usersFilePath, users)
+  return { userName: reqUserName, token: newToken }
+}
+
+const checkToken = async request => {
+  console.log('/api/checkTokenが呼ばれた')
+
+  const { userName: reqUserName, token: reqToken } = await parseReqBody(request)
+  const users = await getDatabase(usersFilePath)
+  const targetIndex = users.findIndex(user => user.userName === reqUserName)
+  if (users[targetIndex].token !== reqToken) {
+    throw { errMsg: `認証期限切れ。ログインし直してください` }
+  }
+  return null
+}
+
+const getGallery = async () => {
+  console.log('/api/apiGetGalleryDataが呼ばれた')
+
+  const gallery = await getDatabase(galleryFilePath)
+  return gallery
+}
+
+const uploadPicture = async request => {
+  console.log('/api/UploadPictureが呼ばれた')
+
+  const {
+    picture: { path: tmpFilePath, name: originalFileName },
+    fields: { userName, title, description }
+  } = await getFormData(request)
+  const pictureName = `${getUniqueStr()}${originalFileName.match(/(\.jpg|\.jpeg|\.png|\.gif)$/)[0]}`
+  await renameFile(tmpFilePath, `${pictureDir}${pictureName}`)
+  let gallery = await getDatabase(galleryFilePath)
+  gallery.push({ title, description, pictureName, userName })
+  await writeFile(galleryFilePath, gallery)
+  return gallery
+}
+
+const deletePicture = async request => {
+  console.log('/api/deletePictureが呼ばれた')
+
+  const { pictureName } = await parseReqBody(request)
+  let { gallery, targetIndex } = await getGalleryDataAndFindTargetIndex(pictureName)
+  await removePicture(gallery[targetIndex].pictureName)
+  gallery.splice(targetIndex, 1)
+  await writeFile(galleryFilePath, gallery)
+  return gallery
+}
+
+const updatePicture = async request => {
+  console.log('/api/updataPictureが呼ばれた')
+
+  const {
+    picture: { path: tmpFilePath, name: originalFileName },
+    fields: { title, description, pictureName }
+  } = await getFormData(request)
+  let { gallery, targetIndex } = await getGalleryDataAndFindTargetIndex(pictureName)
+  let newPictureName = pictureName
+  if (tmpFilePath !== undefined) {
+    await removePicture(pictureName)
+    newPictureName = `${getUniqueStr()}${originalFileName.match(/(\.jpg|\.jpeg|\.png|\.gif)$/)[0]}`
+    await renameFile(tmpFilePath, `${pictureDir}${newPictureName}`)
+  }
+  gallery.splice(targetIndex, 1, { title, description, pictureName: newPictureName })
+  await writeFile(galleryFilePath, gallery)
+  return gallery
+}
+
+module.exports = {
+  signIn,
+  logIn,
+  checkToken,
+  getGallery,
+  uploadPicture,
+  deletePicture,
+  updatePicture
+}
---
+++ apiserver/apiServer.js
@@ -0,0 +1,68 @@
+'use strict'
+
+const http = require('http')
+const fs = require('fs')
+const util = require('util')
+const path = require('path')
+const server = http.createServer()
+const fsReadFileP = util.promisify(fs.readFile)
+const api = require('./apiHandlers.js')
+
+server.on('request', (request, response) => {
+  typeof api[request.url.split('/')[2]] === 'function'
+    ? handleApis(request, response)
+    : sendBackStaticFiles(request, response)
+})
+
+server.listen(3000, () => console.log('server listening'))
+
+const handleApis = async (request, response) => {
+  console.log('"handleApi"通過')
+
+  const responseBody = await api[request.url.split('/')[2]](request)
+    .then(body => {
+      return { status: true, body }
+    })
+    .catch(({ err, place, errMsg: body }) => {
+      err && logError(err, place)
+      return { status: false, body }
+    })
+
+  response.end(JSON.stringify(responseBody))
+}
+
+const sendBackStaticFiles = async (request, response) => {
+  console.log(`${request.url}にアクセスされた`)
+  const url = `dist${request.url.endsWith('/') ? request.url + 'index.html' : request.url}`
+  const getHeadInfos = url => {
+    const types = {
+      '.html': 'text/html',
+      '.js': 'text/javascript',
+      '.jpg': 'image/jpg',
+      '.ico': 'image/ico'
+    }
+    for (const key in types) {
+      if (url.endsWith(key)) return { statusCode: 200, type: types[key] }
+    }
+    return { statusCode: 404, type: 'text/plain' }
+  }
+  const responseBody = await fsReadFileP(url).catch(() => '404 NOT FOUND')
+  response.writeHead(getHeadInfos(url).statusCode, { 'Content-Type': getHeadInfos(url).type })
+  response.end(responseBody)
+}
+
+const logError = (err, { file, name, line, column }) => {
+  console.log('ログエラー')
+  const now = new Date()
+  const year = now.getFullYear()
+  const mon = `0${now.getMonth() + 1}`.slice(-2)
+  const day = `0${now.getDate()}`.slice(-2)
+  const hour = `0${now.getHours()}`.slice(-2)
+  const min = `0${now.getMinutes()}`.slice(-2)
+  const sec = `0${now.getSeconds()}`.slice(-2)
+  const time = `${year}/${mon}/${day} ${hour}:${min}.${sec}`
+  const log = `-----------< ${time} ${file} ${name} ${line}/${column} >--------------\n${err}\n\n`
+  fs.appendFile(path.join(__dirname, '../data/logs/errorLog.txt'), log, 'utf8', () => {
+    return
+  })
+}
