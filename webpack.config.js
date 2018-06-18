253cedb49f52334c99fd8995e54ce3881ce47e09
Yasushi Asahi <asahi@MBP.local>
2018-06-19 03:39:14 +0900

    nginx Container

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,80 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const isProduction = process.env.NODE_ENV === 'production'
 
+isProduction?
 module.exports = {
-  mode: isProduction ? 'production' : 'development',
-  devtool: isProduction && 'inline-source-map',
+  mode: 'production',
 
-  devServer: isProduction && {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  'env',
+                  {
+                    modules: false,
+                    targets: {
+                      node: 'current'
+                    }
+                  }
+                ],
+                'react'
+              ],
+              plugins: ['react-hot-loader/babel', 'babel-plugin-styled-components']
+            }
+          },
+        ],
+        exclude: /node_modules/
+      },
+
+      {
+        test: /\.jpg$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images/',
+              publicPath: path => {
+                return `./images/${path}`
+              }
+            }
+          }
+        ]
+      },
+
+      {
+        test: /\.(png|svg)$/,
+        use: [
+          {
+            loader: 'url-loader'
+          }
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'フワフワ！美容室',
+      template: './src/index.html',
+      favicon: './src/favicon.ico'
+    })
+  ]
+}
+
+:
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+
+  devServer: {
     contentBase: 'dist',
     open: true,
     port: 8080,
@@ -38,7 +107,7 @@ module.exports = {
               plugins: ['react-hot-loader/babel', 'babel-plugin-styled-components']
             }
           },
-          isProduction && 'stylelint-custom-processor-loader'
+          'stylelint-custom-processor-loader'
         ],
         exclude: /node_modules/
       },
