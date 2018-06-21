58a3f99c5f1d50fc3d4de42277ccf8b519af05b2
Yasushi Asahi <asahi1600@gmail.com>
2018-06-22 04:39:35 +0900

    LInk null

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,15 +2,16 @@ import React from 'react'
 import styled from 'styled-components'
 import { properties } from './styles.js'
 
-const Link = ({ componentName, src, alt }) => (
-  <li
-    key={componentName}
-    onClick={() => {
-      location.hash = `#${componentName}`
-    }}>
-    <Img src={src} alt={alt} />
-  </li>
-)
+const Link = ({ componentName, src, alt }) =>
+  location.hash.slice(1) === componentName ? null : (
+    <li
+      key={componentName}
+      onClick={() => {
+        location.hash = `#${componentName}`
+      }}>
+      <Img src={src} alt={alt} />
+    </li>
+  )
 
 const Img = styled.img`
   box-shadow: ${properties.boxShadow()};
--- src/components/MainViewSwitcher.js
+++ src/components/MainViewSwitcher.js
@@ -8,16 +8,14 @@ import goaisatsu from './../images/assets/goaisatsu.svg'
 import menyu from './../images/assets/menyu.svg'
 import omise from './../images/assets/omisezyouhou.svg'
 
-const h = location.hash.slice(1)
-
 const MainViewSwitcher = () => (
   <Ul>
-    {h === 'Home' || <Link componentName="Home" src={homu} alt="ホームリンク" />}
-    {h === 'Greeting' || <Link componentName="Greeting" src={goaisatsu} alt="ご挨拶リンク" />}
-    {h === 'SalonInfo' || <Link componentName="SalonInfo" src={omise} alt="お店情報リンク" />}
-    {h === 'Menu' || <Link componentName="Menu" src={menyu} alt="メニューリンク" />}
-    {h === 'BlogIndex' || <Link componentName="BlogIndex" src={burogu} alt="ブログリンク" />}
-    {h === 'Gallery' || <Link componentName="Gallery" src={garou} alt="画廊リンク" />}
+    <Link componentName="Home" src={homu} alt="ホームリンク" />
+    <Link componentName="Greeting" src={goaisatsu} alt="ご挨拶リンク" />
+    <Link componentName="SalonInfo" src={omise} alt="お店情報リンク" />
+    <Link componentName="Menu" src={menyu} alt="メニューリンク" />
+    <Link componentName="BlogIndex" src={burogu} alt="ブログリンク" />
+    <Link componentName="Gallery" src={garou} alt="画廊リンク" />
   </Ul>
 )
 
